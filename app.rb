e40afdb3c1a0af580e698693e78cd483301da1a4
Natalie Eisner <natalieeisner5@gmail.com>
2018-07-17 13:33:40 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "It was so awesome to see Karlie yesterday! "
+    "It was so awesome to see Karlie yesterday! She put a lot of our pictures and videos on her insta!"
   end
 
 end
